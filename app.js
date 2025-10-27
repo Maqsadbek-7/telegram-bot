bb0626459371d225c5c3e92d6e1676ac14f9b55e
Maksadbek <xxxscrityxxx@gmail.com>
2025-10-27 18:17:43 +0500

    qnopka qoyildi

--- app.js
+++ app.js
@@ -4,6 +4,26 @@ const bot = new TelegramBot(TOKEN, {polling: true});
 bot.on("message", function (msg) {
     
     const chatId = msg.chat.id;
+    const text = msg.text;
+    const firstname = msg.chat.first_name;
+      if (msg.text == "/start") {
+    bot.sendMessage(chatId, `Xush kelibsiz, ${firsname}`, {
+      reply_markup: {
+        keyboard: [
+          [{ text: "Boshlash" }][{ text: "Sozlamalar" }][{ text: "Menu" }],
+        ],
+           resize_keyboard: true,
+         },
+    });
+        }else if(text == "Boshlash")
+        {
+        bot.sendMessage(chatId, "bOSHLANDI");
+        }else if(text == "Menu")
+    bot.sendMessage(chatId, "mENU");
+        {
+  }else if(text == "Sozlamalar")
+  {
+    bot.sendMessage(chatId, "Sozlamalar")
+  }
+});
 
-    bot.sendMessage(chatId, "Salom");
-}); 
